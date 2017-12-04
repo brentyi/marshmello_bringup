49920793c16d5973d6f2a74915df2f46b93f5749
Brent Yi <yibrenth@gmail.com>
2017-12-04 07:07:16 -0800

    tweaks

--- run.sh
+++ run.sh
@@ -6,8 +6,11 @@ tmux send-keys 'roslaunch marshmello_bringup sawyer_moveit.launch'
 tmux new-window -t marshmello -n 'rviz'
 tmux send-keys 'roslaunch marshmello_bringup sawyer_rviz.launch'
 
-tmux new-window -t marshmello -n 'kinect'
-tmux send-keys 'roslaunch marshmello_bringup kinect_localization.launch'
+tmux new-window -t marshmello -n 'camera_drivers'
+tmux send-keys 'roslaunch marshmello_bringup camera_drivers.launch'
+
+tmux new-window -t marshmello -n 'calib'
+tmux send-keys 'roslaunch marshmello_bringup kinect_calibration.launch'
 
 tmux new-window -t marshmello -n 'face'
 tmux send-keys 'roslaunch marshmello_bringup face_detection.launch'
